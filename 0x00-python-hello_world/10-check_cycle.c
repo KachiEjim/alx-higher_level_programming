f7ede6e97ee3762b93cffbf53f2a8d3813f92b57
root <root@EJIMOVIC>
2023-10-30 23:35:34 +0100

    10-check_cycle.c || task 10 upload

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a listint has a cycle
+ *
+ * @list: pointer listint element
+ *
+ * Return: returns 1 if a cycle is found and
+ *          zero otherwise
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *check;
+
+	if (list == NULL)
+		return (0);
+	check = list;
+	temp = list->next;
+	while (temp)
+	{
+		if (check == temp)
+			return (1);
+		temp = temp->next;
+	}
+}
