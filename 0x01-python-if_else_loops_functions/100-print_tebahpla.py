59118bc408e040e27be3dedb137e57daa20f779e
root <root@EJIMOVIC>
2023-11-01 01:00:56 +0100

    correction on task 101

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for character in range(122, 96, -1):
     if character % 2 != 0:
         character = character - 32
-    print(f"{character:c}", end="")
+    print("{:c}".format(character), end="")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,6 +4,7 @@ def remove_char_at(str, n):
     i = 0
     for c in str:
         if i == n:
+            i += 1
             continue
         copy += c
         i += 1
