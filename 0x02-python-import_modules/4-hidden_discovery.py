8d8a38b6a178b003deb5ce35967dbfd29f81a245
root <root@EJIMOVIC>
2023-11-03 11:06:00 +0100

    made some changes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 import hidden_4
 
-
 def main():
-    i = 0
     names = dir(hidden_4)
     for name in sorted(names):
-        if name[i][:2] != '__':
+        if not name.startswith('__'):
             print(name)
-        i += 1
-
 
 if __name__ == "__main__":
     main()
