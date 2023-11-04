d2950a0f0367f7e3c228cc444b337eea62bdc6f4
root <root@EJIMOVIC>
2023-11-04 18:31:08 +0100

    task 13 upload

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,51 +1,71 @@
 #include "lists.h"
 
-/**
- * is_palindrome - checks if a singly linked list is a palindrome
- *
- * @head: double pointer to the head of the singly linked list
- *
- * Return: 0 if not palindrome, 1 if palindrome
- *
- */
 
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome- 0.
+ *If the linked list is a palindrome - 1.
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp;
-		int *items;
-		int len = 0, i;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	temp = *head;
-	while (temp)
+	tmp = *head;
+	while (tmp)
 	{
-		len++;
-		temp = temp->next;
+		size++;
+		tmp = tmp->next;
 	}
 
-	items = malloc(sizeof(int) * len);
-	if (items == NULL)
-	return (0);
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-	temp = *head;
-	for (i = 0; temp; i++)
-	{
-		items[i] = temp->n;
-		temp = temp->next;
-	}
-	temp = *head;
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
 
-	for (i = 0; i < len; i++, len--)
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		if (items[len - 1] != items[i])
-		{
-			free(items);
+		if (tmp->n != rev->n)
 			return (0);
-		}
-	temp = temp->next;
+		tmp = tmp->next;
+		rev = rev->next;
 	}
-	free(items);
+	reverse_listint(&mid);
+
 	return (1);
 }
