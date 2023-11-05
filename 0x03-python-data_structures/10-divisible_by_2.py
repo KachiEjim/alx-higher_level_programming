2f29a3568802efede31c3a3c7462e3afa3c41e94
root <root@EJIMOVIC>
2023-11-05 16:15:43 +0100

    12-switch.py || task 12 upload

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@
 def divisible_by_2(my_list=[]):
     new = []
     for i in range(len(my_list)):
-        if my_list[i] / 2 == 0:
+        if my_list[i] % 2 == 0:
             new.append(True)
         new.append(False)
     return (new)
