d8f7ff8e53b8c1fd854264f6829fad275d7ebdac
root <root@EJIMOVIC>
2023-11-20 22:16:23 +0100

    made pycodestyle happy

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,12 +3,12 @@
 from sys import stderr
 
 def safe_function(fct, *args):
-	arg = []
-	for i in range(len(args)):
-		arg.append(args[i])
-	try:
-		result = fct(arg[0], arg[1])
-	except Exception as e:
-		stderr.write("Exception: {}\n".format(e))
-		return (None)
-	return (result)
+    arg = []
+    for i in range(len(args)):
+        arg.append(args[i])
+    try:
+        result = fct(arg[0], arg[1])
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        return (None)
+    return (result)
