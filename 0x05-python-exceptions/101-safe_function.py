c6b4a6f0a6a15e44c694ff2e53774a3698d4e222
root <root@EJIMOVIC>
2023-11-20 22:38:41 +0100

    Still on code stylr error correction

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,11 +5,8 @@ from sys import stderr
 
 def safe_function(fct, *args):
 
-    arg = []
-    for i in range(len(args)):
-        arg.append(args[i])
     try:
-        result = fct(arg[0], arg[1])
+        result = fct(args)
     except Exception as e:
         stderr.write("Exception: {}\n".format(e))
         return (None)
