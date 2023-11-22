f0a02caf6cd8f65bf0c3abbea717ae7ba60b4311
root <root@EJIMOVIC>
2023-11-22 01:52:52 +0100

    Making corrections

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -57,6 +57,12 @@ class Square:
                 print(((" " * self.__position[0]) + ("#" * self.__size)))
 
     def __str__(self):
-        """Prints same message as my_print"""
-        self.my_print()
+        """ __str__have the same behavior as my_print()."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
         return ("")
