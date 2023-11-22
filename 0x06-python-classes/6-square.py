fdf3d1aedcb121b6482c5678f293699824214545
root <root@EJIMOVIC>
2023-11-22 01:21:05 +0100

    Making corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Define a class Square."""
 
 
 class Square:
@@ -17,13 +17,12 @@ class Square:
 
     @property
     def size(self):
-        """Get the current size of the square."""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Get the current size of the square."""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
@@ -31,15 +30,14 @@ class Square:
 
     @property
     def position(self):
-        """Get the current position of the square."""
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
     def position(self, value):
-        """Set the position of the square."""
-        if ((type(value) != tuple) or
+        if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                all(type(num) != int for num in value) or
+                not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
@@ -51,10 +49,10 @@ class Square:
     def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
-            print()
+            print("")
             return
 
-        [print() for i in range(0, self.__position[1])]
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
