11f2c2e7fe12a70e3c527a42f5b03e939f43ed98
root <root@EJIMOVIC>
2023-11-28 00:16:02 +0100

    All tasks upload

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -70,5 +70,6 @@ class Rectangle:
     def __del__(self):
         """Print the message Bye rectangle...
         when an instance of Rectangle is deleted"""
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
