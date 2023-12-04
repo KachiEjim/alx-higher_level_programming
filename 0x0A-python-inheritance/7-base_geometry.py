a1eb1a9b0488e94a0e47a7638cc6e1567f2e5271
root <root@EJIMOVIC>
2023-12-05 00:09:33 +0100

    Still fighting with task 1 and 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,9 @@ class BaseGeometry:
 
     def area(self):
         """Public instance method: def area(self): that
-        raises an Exception with the message area() is not implemented"""
+        raises an Exception with the message area() is not implemented
+        Exception: area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
