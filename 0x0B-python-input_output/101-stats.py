32b452be9991905d7e6c8265d9f13ad8b5efa362
root <root@EJIMOVIC>
2023-12-07 08:51:59 +0100

    upload

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -17,9 +17,14 @@ def computes_metrics():
     status_code_counts = {200: 0, 301: 0,
                           400: 0, 401: 0, 403: 0,
                           404: 0, 405: 0, 500: 0}
-    line_count = 0
 
     try:
+        line_count = 1
+        if line_count % 10 == 0:
+            print(f"File size: {total_file_size}")
+            for code, count in sorted(status_code_counts.items()):
+                if count > 0:
+                    print(f"{code}: {count}")
         for line in sys.stdin:
             line_count += 1
             parts = line.split()
@@ -42,11 +47,7 @@ def computes_metrics():
             except (ValueError):
                 pass
 
-            if line_count % 10 == 0:
-                print(f"File size: {total_file_size}")
-                for code, count in sorted(status_code_counts.items()):
-                    if count > 0:
-                        print(f"{code}: {count}")
+
 
     except KeyboardInterrupt:
         print(f"File size: {total_file_size}")
