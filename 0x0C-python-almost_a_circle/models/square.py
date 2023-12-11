bf75eea079cf2f217b79ee413aa4cba547158902
Morishey <Morishey@gmail.com>
2023-12-12 00:26:40 +0100

    Error fond

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -45,6 +45,7 @@ class Square(Rectangle):
                 self.x = args[2]
             if len(args) >= 4:
                 self.y = args[3]
+
         else:
             if kwargs and len(kwargs) != 0:
                 for key, value in kwargs.items():
