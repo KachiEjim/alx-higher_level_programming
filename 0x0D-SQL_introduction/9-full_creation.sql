dc0fb0f44d9eee0200204e7a9e01395c3d4ddc34
ejimvincent <ejimovc@gmail.com>
2024-03-13 06:29:05 -0700

    upload

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,17 @@
 -- SQL script to create the table second_table in the database hbtn_0c_0 and add multiple rows
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
+CREATE TABLE IF NOT EXISTS `second_table`
+(
+    	id INT,
+   	name VARCHAR(256),
+   	score INT
 );
 
 -- Inserting multiple rows into the second_table
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+INSERT INTO `second_table` (id, name, score)
+VALUES
+(
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8)
+);
