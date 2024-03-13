dcd58c0c1355286ef1fc26227cbcd400c8960c60
ejimvincent <ejimovc@gmail.com>
2024-03-13 06:44:39 -0700

    task 10 upload

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
 -- SQL script to list all records of the table second_table ordered by score in descending order
-SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score, name 
+FROM second_table
+ORDER BY score DESC;
