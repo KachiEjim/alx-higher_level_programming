e0af9960808a76ff429d40667f9e8ae849e05c8d
ejimvincent <ejimovc@gmail.com>
2024-03-15 23:10:58 -0700

    upload

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,10 @@
--- Convert the table to the desired character set and collation
+USE hbtn_0c_0;
+
+-- Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Alter the field within the table to the desired character set and collation
+-- Convert the name field to UTF8
 ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
