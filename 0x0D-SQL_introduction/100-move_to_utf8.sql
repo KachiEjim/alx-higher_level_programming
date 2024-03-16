7a6108c6ad91a394d7bc9fefca360a13717fec3c
ejimvincent <ejimovc@gmail.com>
2024-03-15 23:18:49 -0700

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- a script that converts hbtn_0c_0 database to UTF8
-
+-- a script that converts hbtn_0c_0 database to UTF8.
 -- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
