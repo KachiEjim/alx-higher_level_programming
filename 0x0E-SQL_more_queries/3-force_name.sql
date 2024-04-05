83be9ac629f0187a083bbddec22f065287e5c2b4
ejimvincent <ejimovc@gmail.com>
2024-04-05 12:07:13 +0100

    upload

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,2 @@
 --  a script that creates the force_name table in your MySQL server
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
