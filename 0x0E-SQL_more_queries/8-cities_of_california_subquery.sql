1e0e1fffb46f7585fdc88c6e3cce8514ecb89b1b
ejimvincent <ejimovc@gmail.com>
2024-04-07 12:00:53 +0100

    task 7

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- a script that creates the database hbtn_0d_usa and the table cities
+SELECT id, name
+FROM city
+WHERE state_id = (SELECT id FROM state WHERE name = 'California')
+ORDER BY id ASC;
