972a7e4012e23b6088f389b96d336813be1ff794
ejimvincent <ejimovc@gmail.com>
2024-04-09 00:10:08 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let argv = process.argv;
+let occur = Number(argv[2]);
+
+if (isNaN(occur)) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 0; i < occur; i++) {
+        console.log('C is fun');
+    }
+}
