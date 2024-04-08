7b0e8f3a5414d8163ce793252a43b3848252497f
ejimvincent <ejimovc@gmail.com>
2024-04-08 23:29:19 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let argv = process.argv;
-let num = Number(argv[2]);
+const argv = process.argv;
+const num = Number(argv[2]);
 
 if (isNaN(num)) {
   console.log('Not a number');
