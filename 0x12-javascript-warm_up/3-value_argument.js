98314cf7527971289bebc2754d68f3f9371af91f
ejimvincent <ejimovc@gmail.com>
2024-04-08 23:12:07 +0100

    task 0

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-const argv = process.argv
-if (argv[2] === undefined)
-{
-    console.log('No argument');
-}
-else
-{
-    console.log(argv[2]);
-}
+const { argv } = require('process');
+if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
