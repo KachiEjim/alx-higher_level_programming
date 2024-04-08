0f3f93fb4759197ddf41d447bddd71a6d902ea28
ejimvincent <ejimovc@gmail.com>
2024-04-08 23:26:46 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,9 @@
 
 let argv = process.argv;
 let num = Number(argv[2]);
+
 if (isNaN(num)) {
-     console.log('Not a number'); 
-}
-else {
-    console.log(`My number: ${num}`);
+  console.log('Not a number');
+} else {
+  console.log('My number: ${num}');
 }
