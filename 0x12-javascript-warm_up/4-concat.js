d7890d7b4d57f272d8ef284ad6c259ae7dd6ff79
ejimvincent <ejimovc@gmail.com>
2024-04-08 23:24:16 +0100

    task 0

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const argv  = process.argv;
-const argv1 = argv[2]; 
+
+const argv = process.argv;
+const argv1 = argv[2];
 const argv2 = argv[3];
+
 console.log(argv1 + ' is ' + argv2);
