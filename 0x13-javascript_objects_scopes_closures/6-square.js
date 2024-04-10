b57298fc79df99a78845e9c2ebc6b41919bb47cb
ejimvincent <ejimovc@gmail.com>
2024-04-10 01:43:14 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
-
+#!/usr/bin/node
 
 const Square5 = require('./5-square');
 
 module.exports = class Square extends Square5 {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
-      let c = 'X';
+      const c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
   }
-}
+};
