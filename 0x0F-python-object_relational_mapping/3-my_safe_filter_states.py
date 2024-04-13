bbc807a7ce4ba6993d1dbf28781a17fd1858c4a0
ejimvincent <ejimovc@gmail.com>
2024-04-13 10:55:54 +0100

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""A script to list all states from the hbtn_0e_0_usa database.
+Usage:
+    python list_states.py <username> <password> <database>
+"""
+
+import MySQLdb
+from sys import argv
+
+
+
+def listStates(username, password, database, state_name):
+    """List all states from the hbtn_0e_0_usa database.
+    Args:
+        username (str): The MySQL username.
+        password (str): The MySQL password.
+        database (str): The name of the MySQL database.
+        state_name (str): The name of the state to be displayed
+
+    Returns: None
+        """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(argv) == 5:
+        username = argv[1]
+        password = argv[2]
+        database = argv[3]
+        state_name = argv[4]
+        listStates(username, password, database, state_name)
