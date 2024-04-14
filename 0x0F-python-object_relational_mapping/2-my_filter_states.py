65019b5ee9150020e0230de1c10034398c9e9a8c
ejimvincent <ejimovc@gmail.com>
2024-04-14 11:49:00 +0100

    make pycodestyle happy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -27,8 +27,8 @@ def listStates(username, password, database, state_name):
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' \
-    ORDER BY id".format(state_name)
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' \
+    ORDER BY id".format(state_name.strip("'\""))
     cursor.execute(query)
     rows = cursor.fetchall()
 
