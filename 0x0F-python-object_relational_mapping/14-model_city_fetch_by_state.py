25345155121831e15221c2c377079610f2dd1cf6
ejimvincent <ejimovc@gmail.com>
2024-04-15 15:59:14 +0100

    upload

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,13 +12,15 @@ def fetchCity(uname, pword, db):
     """prints all City objects from the database hbtn_0e_14_usa:"""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         uname, pword, db), pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City, State).filter(City.state_id == State.id).order_by(City.id)
+    cities = session.query(City, State)\
+        .filter(City.state_id == State.id).order_by(City.id)
     for city, state in cities:
         print(f"{state.name}: ({city.id}) {city.name}")
+
 
 if __name__ == "__main__":
     u = argv[1]
