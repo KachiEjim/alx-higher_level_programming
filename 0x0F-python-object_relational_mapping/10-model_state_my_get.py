504bebbad610781bb87973fbf4d2e138be97ec04
ejimvincent <ejimovc@gmail.com>
2024-04-15 01:55:12 +0100

    upload

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa.
-Usage: ./9-model_state_filter_a.py <mysql username> /
-                                   <mysql password> /
-                                   <database name>
+Prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa.
 """
-from sys import argv
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 
-def fetchWithA(username, password, dbname):
-    """ that lists all State objects that contain the
-    letter a from the database hbtn_0e_6_usa
-
+def fetchStateByName(username, password, dbname, state_name):
+    """Fetches the State object by name from the database.
     Attributes:
         username (str): Username of the user
         password (str): user password
         dbname (str): Database to be assessed
+        state_name (str): name of the state
     """
+
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(username, password, dbname),
                            pool_pre_ping=True)
-    Session_maker = sessionmaker(bind=engine)
-    session = Session_maker()
-    states = session.query(State).order_by(State.id)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State)
     for state in states:
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+        if state_name == state.name:
+            print("{}".format(state.id))
+            break
+    else:
+        print("Not found")
 
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    dbname = argv[3]
-    fetchWithA(username, password, dbname)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    fetchStateByName(username, password, dbname, state_name)
