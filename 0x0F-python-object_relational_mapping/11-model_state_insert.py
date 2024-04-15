f366adc54be77de22d3e8eeada40eda16575ca7a
ejimvincent <ejimovc@gmail.com>
2024-04-15 08:09:49 +0100

    upload

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,12 +8,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
-def addObj(uname, pword, db):
-    """a class that adds the State object
-    “Louisiana” to the database hbtn_0e_6_usa"""
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(uname, pword, db),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session_maker = sessionmaker(bind=engine)
@@ -22,12 +19,4 @@ def addObj(uname, pword, db):
     obj = State(name="Louisiana")
     session.add(obj)
     session.commit()
-    session.close()
     print(obj.id)
-
-
-if __name__ == "__main__":
-    uname = argv[1]
-    pword = argv[2]
-    db = argv[3]
-    addObj(uname, pword, db)
