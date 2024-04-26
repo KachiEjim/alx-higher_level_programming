34779243d9bbaac926bee8596c0e0752cb10695c
ejimvincent <ejimovc@gmail.com>
2024-04-26 20:28:40 +0100

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Retrieve header with requests module
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    r = requests.get(url)
+    header = r.headers['X-Request-Id']
+    print(header)
