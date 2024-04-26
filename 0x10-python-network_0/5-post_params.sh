98fc149120d1cca0405e29a4d1f014990513a316
ejimvincent <ejimovc@gmail.com>
2024-04-26 04:58:26 +0100

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display response body
-curl -s "$1" -X GET -H "X-school-User-Id: 98"
+# Make POST request, display response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
