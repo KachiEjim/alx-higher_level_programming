244839cfc0788ca54e2eabc49419a4ad21448abb
ejimvincent <ejimovc@gmail.com>
2024-04-26 20:31:39 +0100

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Retrieve header 'X-Request-Id'
-with requests module
+"""Retrieve header with requests module
 """
 
 import requests
@@ -10,5 +9,5 @@ from sys import argv
 if __name__ == '__main__':
     url = argv[1]
     r = requests.get(url)
-    header = r.headers['X-Request-Id']
-    print(header)
+    header = r.headers
+    print(header.get('X-Request-Id'))
