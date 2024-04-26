d4efc5cceb46463a980717da76c440a6a1a71196
ejimvincent <ejimovc@gmail.com>
2024-04-26 20:56:13 +0100

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Display response body or error message
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    url = argv[1]
+
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        print(result.text)
+    except:
+        print('Error code: {}'.format(result.status_code))
