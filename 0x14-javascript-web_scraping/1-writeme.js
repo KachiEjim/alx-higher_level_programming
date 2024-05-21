584eb3c8372ccac7dd6f67df10b670a2ef9f4df7
ejimvincent <ejimovc@gmail.com>
2024-05-21 21:25:13 +0100

    FIX(0x14-javascript-web_scraping:Task 1) Added retrival for second argument

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,6 @@ const file = process.argv[2];
 const data = process.argv[3];
 try {
   fs.writeFileSync(file, data, 'utf8');
-  console.log('File written successfully');
 } catch (err) {
   console.log(err);
 }
