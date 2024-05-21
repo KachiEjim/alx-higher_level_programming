1a7f7653b863c4603f2aaad9834c2dbd4cb2fd68
ejimvincent <ejimovc@gmail.com>
2024-05-21 21:24:08 +0100

    FIX(0x14-javascript-web_scraping:Task 1) Added retrival for second argument

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const file = process.argv[2];
-
+const data = process.argv[3];
 try {
   fs.writeFileSync(file, data, 'utf8');
   console.log('File written successfully');
