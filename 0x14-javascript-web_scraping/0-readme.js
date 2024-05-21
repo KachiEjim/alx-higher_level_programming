ab3e465fd9e73a878a3d081783c54daaceddde80
ejimvincent <ejimovc@gmail.com>
2024-05-21 20:23:03 +0100

    BUILD(0x14-javascript-web_scraping:Task 0)reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-file = process.argv[2]
+
+const fs = require('fs');
+
+const file = process.argv[2];
+try {
+  const data = fs.readFileSync(file, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
